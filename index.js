3d9ff1591dc00b40ba76d0a36cf0c79ea4003606
Andres Montes <amontes@merqueo.com>
2021-03-19 07:09:04 -0500

    feat: test guild

---
+++ index.js
@@ -0,0 +1,18 @@
+window.CSS.registerProperty({
+	name: "--primary",
+	syntax: "<color>",
+	inherits: false,
+	initialValue: "#d93178",
+});
+window.CSS.registerProperty({
+	name: "--secondary",
+	syntax: "<color>",
+	inherits: false,
+	initialValue: "#ab1aa4",
+});
+window.CSS.registerProperty({
+	name: "--tertiary",
+	syntax: "<color>",
+	inherits: false,
+	initialValue: "#323c8f",
+});
